74355c3a7163602923f9f8444b403b7bdafaa163
gerges bernaba <gergesbernaba2@gmail.com>
2023-02-22 15:38:58 +0200

    split file and send it

---
+++ uploadFileV2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace uploadFileV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "List of File name:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(232, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(192, 28);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(181, 173);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(167, 26);
+            this.progressBar1.TabIndex = 14;
+            this.progressBar1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Path: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 35);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Browse Images";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(501, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 39);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Merg file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ uploadFileV2/Form1.cs
@@ -0,0 +1,260 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace uploadFileV2
+{
+    public partial class Form1 : Form
+    {
+        static string filename;
+        public byte[] FileZIPArray;
+        string SaveFileFolder = @"D:/Images/VIP";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.InitialDirectory = "C://Desktop";
+            openFileDialog1.Title = "Select file to be upload.";
+
+            openFileDialog1.FilterIndex = 1;
+            try
+            {
+
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //check size file less than 100 MB
+                    var fileInfo = new FileInfo(openFileDialog1.FileName);
+                    if (fileInfo.Length > 9.9e+7)
+                    {
+                        MessageBox.Show("you file greater than 99 MB");
+                        label2.Text = null;
+                    }
+                    if (openFileDialog1.CheckFileExists)
+                    {
+                        string path = System.IO.Path.GetFullPath(openFileDialog1.FileName);
+                        filename = fileInfo.Name;
+                        label2.Text = path;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Upload file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+            //using (FileStream csvStream = File.Open(label2.Text, FileMode.Open, FileAccess.Read))
+            //{
+            //    using (MemoryStream zipToCreate = new MemoryStream())
+            //    {
+            //        using (ZipArchive archive = new ZipArchive(zipToCreate, ZipArchiveMode.Create, true))
+            //        {
+            //            ZipArchiveEntry fileEntry = archive.CreateEntry(Path.GetFileName(label2.Text));
+            //            using (var entryStream = fileEntry.Open())
+            //            {
+            //                csvStream.CopyTo(entryStream);
+            //            }
+
+            //        }
+
+            //        FileZIPArray = zipToCreate.ToArray();
+            //    }
+            //}
+
+
+            string filepath = label2.Text;
+            Array FileZIPArray = File.ReadAllBytes(filepath);
+            string Part = "Part";
+
+            int partSize = FileZIPArray.Length / 3;
+
+            int Count = 0;
+            for (int i = 0, j = partSize; j <= FileZIPArray.Length; i = j, j += partSize)
+            {
+                Count++;
+                //get remain byte
+                if (Count == 3)
+                {
+                    partSize = FileZIPArray.Length - (partSize * 2);
+                }
+                byte[] result = new byte[partSize];
+                Array.Copy(FileZIPArray, i, result, 0, partSize);
+                string partasbase64String = Convert.ToBase64String(result);
+                using (var stream = new FileStream($"D:/AfterParts_{filename}", FileMode.Append))
+                {
+                    stream.Write(result, 0, result.Length);
+                }
+
+                ///   Post(partasbase64String, Part + Count + "_" + filename, "https://histocrapi.expertapps.com.sa/api/WsiManager/UploadWsiPart");
+
+            }
+
+
+        }
+        static double ConvertBytesToMegabytes(long bytes)
+        {
+            return (bytes / 1024f) / 1024f;
+        }
+      
+        public void Post(string attachment, string partName, string uri)
+        {
+            var body = new { attachment = attachment, partName = partName };
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.Timeout = System.Threading.Timeout.Infinite;
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string AuditDtoJson = Newtonsoft.Json.JsonConvert.SerializeObject(body);
+                streamWriter.Write(AuditDtoJson);
+                streamWriter.Flush();
+            }
+
+            httpWebRequest.GetResponse();
+            //using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            //{
+            //    var result = streamReader.ReadToEnd();
+            //    return true;
+            //}
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles("D:/Images");
+            ComboBox cb = new ComboBox();
+            cb.Location = new Point(10, 22);
+            cb.Size = new Size(200, 30);
+            foreach (string file in files)
+            {
+                cb.Items.Add(Path.GetFileName(file));
+            }
+            this.Controls.Add(cb);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           // string sourceDir = @"D:/Images";
+           // string backupDir = @"D:/Images/VIP";
+           // MergeFile(sourceDir);
+           // DirectoryInfo dirInfo = new DirectoryInfo(backupDir);
+
+           // DirectorySecurity dirSecurity = dirInfo.GetAccessControl();
+           // dirSecurity.AddAccessRule(new FileSystemAccessRule("Gerges Bernaba",
+           //     FileSystemRights.Read | FileSystemRights.Write, AccessControlType.Allow));
+           // dirInfo.SetAccessControl(dirSecurity);
+
+           //// CombineMultipleFilesIntoSingleFile(sourceDir, "*.png", backupDir);
+           // string[] picList = Directory.GetFiles(sourceDir, "*.png");
+
+           // foreach (string f in picList)
+           // {
+           //     // Remove path from the file name.
+           //     string fName = f.Substring(sourceDir.Length + 1);
+
+           //     // Use the Path.Combine method to safely append the file name to the path.
+           //     // Will overwrite if the destination file already exists.
+           //     File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName), true);
+           // }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        //public bool MergeFile(string inputfoldername1)
+        //{
+        //    bool Output = false;
+        //    try
+        //    {
+        //        string[] tmpfiles = Directory.GetFiles(inputfoldername1, "*.png");
+        //        FileStream outPutFile = null;
+        //        string PrevFileName = "";
+        //        foreach (string tempFile in tmpfiles)
+        //        {
+        //            string fileName = Path.GetFileNameWithoutExtension(tempFile);
+        //          //  string baseFileName = fileName.Substring(0, fileName.IndexOf(Convert.ToChar(".")));
+        //            string extension = Path.GetExtension(fileName);
+        //            if (!PrevFileName.Equals(fileName))
+        //            {
+        //                if (outPutFile != null)
+        //                {
+        //                    outPutFile.Flush();
+        //                    outPutFile.Close();
+        //                }
+        //                outPutFile = new FileStream(SaveFileFolder + "\\" + fileName + extension, FileMode.OpenOrCreate, FileAccess.Write);
+        //            }
+        //            int bytesRead = 0;
+        //            byte[] buffer = new byte[1024];
+        //            FileStream inputTempFile = new FileStream(tempFile, FileMode.OpenOrCreate, FileAccess.Read);
+        //            while ((bytesRead = inputTempFile.Read(buffer, 0, 1024)) > 0)
+        //                outPutFile.Write(buffer, 0, bytesRead);
+        //            inputTempFile.Close();
+        //            File.Delete(tempFile);
+        //            PrevFileName = fileName;
+        //        }
+        //        outPutFile.Close();
+        //        label5.Text = "Files have been merged and saved at location C:\\";
+        //    }
+        //    catch
+        //    {
+        //    }
+        //    return Output;
+
+        //}
+
+        //private static void CombineMultipleFilesIntoSingleFile(string inputDirectoryPath, string inputFileNamePattern, string outputFilePath)
+        //{
+
+        //    string[] inputFilePaths = Directory.GetFiles(inputDirectoryPath, inputFileNamePattern);
+        //    Console.WriteLine("Number of files: {0}.", inputFilePaths.Length);
+
+        //    //DirectoryInfo dirInfo = new DirectoryInfo(outputFilePath);
+
+        //    //DirectorySecurity dirSecurity = dirInfo.GetAccessControl();
+        //    //dirSecurity.AddAccessRule(new FileSystemAccessRule("Gerges Bernaba",
+        //    //    FileSystemRights.Read | FileSystemRights.Write, AccessControlType.Allow));
+        //    //dirInfo.SetAccessControl(dirSecurity);
+        //    using (var outputStream = File.Create(outputFilePath))
+        //    {
+        //        foreach (var inputFilePath in inputFilePaths)
+        //        {
+        //            using (var inputStream = File.OpenRead(inputFilePath))
+        //            {
+        //                // Buffer size can be passed as the second argument.
+        //                inputStream.CopyTo(outputStream);
+        //            }
+        //            Console.WriteLine("The file {0} has been processed.", inputFilePath);
+        //        }
+        //    }
+        //}
+    }
+}
